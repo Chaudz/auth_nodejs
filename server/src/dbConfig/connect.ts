50a06ebe9b0441a684dfa7478b69898f230315ba
ChauDz <buichau40@gmail.com>
2024-03-23 19:18:28 +0700

    fix: delete userName index

--- server/src/dbConfig/connect.ts
+++ server/src/dbConfig/connect.ts
@@ -4,11 +4,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.URL_MONGODB}`);
     console.log("connect is successfully");
-
-    await mongoose.connection.db
-      .collection("users")
-      .createIndex({ userName: 1 }, { unique: true });
-    console.log("Index for userName created");
   } catch (error) {
     console.log("connect is fail");
   }
